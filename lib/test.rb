788dc7bc47314c068f2c43f9b34be4620d3d8c3c
ahmed <tou.devpro@gmail.com>
2019-01-16 11:28:39 +0100

    2nd commit

--- lib/test.rb
+++ lib/test.rb
@@ -1,6 +1,6 @@
 require 'dotenv'# Appelle la gem Dotenv
 
-Dotenv.load # Ceci appelle le fichier .env grâce à la gem Dotenv, et importe toutes les données enregistrées dans un hash ENV
+Dotenv.load'../.env' # Ceci appelle le fichier .env grâce à la gem Dotenv, et importe toutes les données enregistrées dans un hash ENV
 
 # Il est ensuite très facile d'appeler les données du fichier .env, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
 puts ENV['TWITTER_API_SECRET']
